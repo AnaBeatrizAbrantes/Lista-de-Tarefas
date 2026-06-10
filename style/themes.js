07f654f35894fa64f7df9fdc705b60142fad1c5a
AnaBeatrizAbrantes <ana.beatriz.dev@gmail.com>
2026-06-10 17:06:13 -0300

    dark theme

--- style/themes.js
+++ style/themes.js
@@ -10,3 +10,16 @@ export const lightTheme = {
   textMuted: '#64748B',
   completed: '#94A3B8'
 };
+
+export const darkTheme = {
+  body: '#0F172A',
+  text: '#F8FAFC',
+  cardBg: '#1E293B',
+  border: '#334155',
+  accent: '#3B82F6',
+  accentHover: '#2563EB',
+  danger: '#F87171',
+  dangerHover: '#EF4444',
+  textMuted: '#94A3B8',
+  completed: '#64748B'
+};
